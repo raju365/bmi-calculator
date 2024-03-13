cf8f91f33c46fd27c838703cb6cb7f8ab0c30d67
raju365 <82221153+raju365@users.noreply.github.com>
2024-03-13 14:55:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,48 @@
+import { useMemo } from "react";
 import { useState } from "react";
 
-import "./App.css";
-
 function App() {
-  const [height, setHeight] = useState(0);
-  const [weight, setWeight] = useState(0);
-  const [message, setMessage] = useState("");
-  const [bmi, setBmi] = useState("");
+  const [height, setHeight] = useState(170);
+  const [weight, setWeight] = useState(40);
 
-  var reload = () => {
-    window.location.reload();
+  const onWeightChange = (e) => {
+    setWeight(e.target.value);
   };
-
-  //calculation logics
-  const calculate = (e) => {
-    e.preventDefault();
-    if (weight == 0 || height == 0) {
-      alert("Please enter a valid height and weight");
-    } else {
-      let bmi = weight / (height * 0.305 * height * 0.305);
-      setBmi(bmi.toFixed(1));
-      if (bmi < 25) {
-        setMessage("You are underweight");
-      } else if (bmi >= 25 && bmi < 30) {
-        setMessage("You are a healthy weight");
-      } else {
-        setMessage("You are overweight");
-      }
-    }
+  const onHeightChange = (e) => {
+    setHeight(e.target.value);
   };
-
+  const output = useMemo(() => {
+    const calculateHeight = height / 100;
+    return (weight / (calculateHeight * calculateHeight)).toFixed(1);
+  }, [weight, height]);
   return (
-    <div className="app">
-      <div className="container">
-        <h2>BMI Calculator</h2>
-        <form onSubmit={calculate}>
-          <div className="hieghtDiv">
-            <label>Weight (kg)</label>
-            <input
-              type="text"
-              placeholder="Enter Weight value"
-              value={weight}
-              onChange={(e) => setWeight(e.target.value)}
-            />
-          </div>
-          <div className="hieghtDiv">
-            <label>Height (m)</label>
-            <input
-              type="text"
-              placeholder="Enter Height value"
-              value={height}
-              onChange={(e) => setHeight(e.target.value)}
-            />
-          </div>
-          <div>
-            <button className="btn" type="submit">
-              Submit
-            </button>
-            <button className="btn btn-outline" onClick={reload} type="submit">
-              Reload
-            </button>
-          </div>
-          <div className="center">
-            <h3>Your BIM is:{bmi} </h3>
-            <p>{message}</p>
-          </div>
-        </form>
+    <main>
+      <h1>BMI calculator</h1>
+      <div className="input-section">
+        <p className="slider-output">Weight:{weight}kg</p>
+        <input
+          type="range"
+          className="input-slider"
+          step="1"
+          min="20"
+          max="200"
+          onChange={onWeightChange}
+        />
+        <p className="slider-output">Height:{height}cm</p>
+        <input
+          type="range"
+          className="input-slider"
+          step="1"
+          min="140"
+          max="220"
+          onChange={onHeightChange}
+        />
+        <div className="output-section">
+          <p>Your BMI is</p>
+          <p className="output">{output}</p>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
